a83f3a933bea8f3957c24ca4c26857418e636f84
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-08-08 11:19:52 -0300

    create ext

--- gauges/deadtuples_test.go
+++ gauges/deadtuples_test.go
@@ -12,6 +12,8 @@ func TestDeadTuples(t *testing.T) {
 	defer close()
 	_, err := db.Exec("CREATE TABLE IF NOT EXISTS testtable(id bigint)")
 	assert.NoError(err)
+	_, err = db.Exec("CREATE EXTENSION IF NOT EXISTS pgstattuple")
+	assert.NoError(err)
 	var metrics = evaluate(t, gauges.DeadTuples())
 	assert.Len(metrics, 1)
 	for _, m := range metrics {
