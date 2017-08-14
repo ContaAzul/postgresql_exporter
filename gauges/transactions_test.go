10377eea2cd3bcae72a8a570dc0da6024ed4b4d2
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-08-14 11:35:51 -0300

    renamed

--- gauges/transactions_test.go
+++ gauges/transactions_test.go
@@ -6,11 +6,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTps(t *testing.T) {
+func TestTransactionsSum(t *testing.T) {
 	var assert = assert.New(t)
 	_, gauges, close := prepare(t)
 	defer close()
-	var metrics = evaluate(t, gauges.Tps())
+	var metrics = evaluate(t, gauges.TransactionsSum())
 	assert.Len(metrics, 1)
 	assertGreaterThan(t, 1, metrics[0])
 }
