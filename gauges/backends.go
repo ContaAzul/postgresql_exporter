b7ce18e6ac83b8599165511f39ba9c8cbced5d18
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-08-14 15:19:31 -0300

    improved backends count

--- gauges/backends.go
+++ gauges/backends.go
@@ -1,6 +1,10 @@
 package gauges
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 func (g *Gauges) Backends() prometheus.Gauge {
 	return g.new(
@@ -32,46 +36,59 @@ func (g *Gauges) MaxBackends() prometheus.Gauge {
 	)
 }
 
+type backendStatus struct {
+	Count float64 `db:"count"`
+	User  string  `db:"usename"`
+	State string  `db:"state"`
+}
+
 func (g *Gauges) BackendsStatus() *prometheus.GaugeVec {
 	var opts = prometheus.GaugeOpts{
 		Name:        "postgresql_backends",
 		Help:        "Active database connections",
 		ConstLabels: g.labels,
 	}
-	var gauge = prometheus.NewGaugeVec(opts, []string{"status"})
-	for _, status := range []string{"active", "idle", "idle in transaction"} {
-		g.from(
-			gauge.With(prometheus.Labels{"status": status}),
-			`
-				SELECT COUNT(*)
-				FROM pg_stat_activity
-				WHERE datname = current_database()
-				AND state = $1
-				AND pid <> pg_backend_pid()
-			`,
-			status,
-		)
-	}
-	g.from(
-		gauge.With(prometheus.Labels{"status": "waiting"}),
-		g.waitingBackendsQuery(),
-	)
+	var gauge = prometheus.NewGaugeVec(opts, []string{"status", "user"})
+	const backendsQuery = `
+		SELECT COUNT(*) as count, state, usename
+		FROM pg_stat_activity
+		WHERE datname = current_database()
+		GROUP BY state, usename
+	`
+	go func() {
+		for _, q := range []string{
+			backendsQuery,
+			g.waitingBackendsQuery(),
+		} {
+			var statuteses []backendStatus
+			g.query(q, &statuteses, emptyParams)
+			for _, status := range statuteses {
+				gauge.With(prometheus.Labels{
+					"status": status.State,
+					"user":   status.User,
+				}).Set(status.Count)
+			}
+		}
+		time.Sleep(g.interval)
+	}()
 	return gauge
 }
 
 func (g *Gauges) waitingBackendsQuery() string {
 	if isPG96(g.version()) {
 		return `
-			SELECT COUNT(*)
+			SELECT COUNT(*) as count, 'waiting' as state, usename
 			FROM pg_stat_activity
 			WHERE datname = current_database()
 			AND wait_event is not null
+			GROUP BY usename
 		`
 	}
 	return `
-		SELECT COUNT(*)
+		SELECT COUNT(*) as count, 'waiting' as state, usename
 		FROM pg_stat_activity
 		WHERE datname = current_database()
 		AND waiting is true
+		GROUP BY usename
 	`
 }
