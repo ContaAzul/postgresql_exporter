d4972f451ff1c8fb78f51c01d3daac4b20a19164
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-08-14 11:35:51 -0300

    renamed

--- gauges/transactions.go
+++ gauges/transactions.go
@@ -2,11 +2,11 @@ package gauges
 
 import "github.com/prometheus/client_golang/prometheus"
 
-func (g *Gauges) Tps() prometheus.Gauge {
+func (g *Gauges) TransactionsSum() prometheus.Gauge {
 	return g.new(
 		prometheus.GaugeOpts{
-			Name:        "postgresql_tps",
-			Help:        "Transactions per second from current database",
+			Name:        "postgresql_transactions_sum",
+			Help:        "Sum of all transactions in the database",
 			ConstLabels: g.labels,
 		},
 		`
