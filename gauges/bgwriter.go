d9885db5b87c6fb84a362d89086ec6abf083a08b
Diogo Nicoleti <diogo.nicoleti@gmail.com>
2018-07-10 14:17:03 -0300

    Improve buffers & checkpoints metrics

--- gauges/bgwriter.go
+++ gauges/bgwriter.go
@@ -2,6 +2,7 @@ package gauges
 
 import "github.com/prometheus/client_golang/prometheus"
 
+// RequestedCheckpoints returns the number of requested checkpoints that have been performed
 func (g *Gauges) RequestedCheckpoints() prometheus.Gauge {
 	return g.new(
 		prometheus.GaugeOpts{
@@ -9,13 +10,11 @@ func (g *Gauges) RequestedCheckpoints() prometheus.Gauge {
 			Help:        "Number of requested checkpoints that have been performed",
 			ConstLabels: g.labels,
 		},
-		`
-			SELECT checkpoints_req
-			FROM pg_stat_bgwriter
-		`,
+		"SELECT checkpoints_req FROM pg_stat_bgwriter",
 	)
 }
 
+// ScheduledCheckpoints returns the number of scheduled checkpoints that have been performed
 func (g *Gauges) ScheduledCheckpoints() prometheus.Gauge {
 	return g.new(
 		prometheus.GaugeOpts{
@@ -23,37 +22,55 @@ func (g *Gauges) ScheduledCheckpoints() prometheus.Gauge {
 			Help:        "Number of scheduled checkpoints that have been performed",
 			ConstLabels: g.labels,
 		},
-		`
-			SELECT checkpoints_timed
-			FROM pg_stat_bgwriter
-		`,
+		"SELECT checkpoints_timed FROM pg_stat_bgwriter",
 	)
 }
 
-func (g *Gauges) BufferOversize() prometheus.Gauge {
+// BuffersMaxWrittenClean returns the number of times the background writer stopped a
+// cleaning scan because it had written too many buffers
+func (g *Gauges) BuffersMaxWrittenClean() prometheus.Gauge {
 	return g.new(
 		prometheus.GaugeOpts{
-			Name:        "postgresql_checkpoints_buffer_oversize",
+			Name:        "postgresql_buffers_maxwritten_clean",
 			Help:        "Number of times the background writer stopped a cleaning scan because it had written too many buffers",
 			ConstLabels: g.labels,
 		},
-		`
-			SELECT maxwritten_clean
-			FROM pg_stat_bgwriter
-		`,
+		"SELECT maxwritten_clean FROM pg_stat_bgwriter",
 	)
 }
 
-func (g *Gauges) BuffersWritten() prometheus.Gauge {
+// BuffersWrittenByCheckpoints returns the number of buffers written during checkpoints
+func (g *Gauges) BuffersWrittenByCheckpoints() prometheus.Gauge {
 	return g.new(
 		prometheus.GaugeOpts{
-			Name:        "postgresql_checkpoints_buffer_written",
+			Name:        "postgresql_buffers_checkpoint",
+			Help:        "Number of buffers written during checkpoints",
+			ConstLabels: g.labels,
+		},
+		"SELECT buffers_checkpoint FROM pg_stat_bgwriter",
+	)
+}
+
+// BuffersWrittenByBgWriter returns the number of buffers written by the background writer
+func (g *Gauges) BuffersWrittenByBgWriter() prometheus.Gauge {
+	return g.new(
+		prometheus.GaugeOpts{
+			Name:        "postgresql_buffers_clean",
+			Help:        "Number of buffers written by the background writer",
+			ConstLabels: g.labels,
+		},
+		"SELECT buffers_clean FROM pg_stat_bgwriter",
+	)
+}
+
+// BuffersWrittenByBackend returns the number of buffers written directly by a backend
+func (g *Gauges) BuffersWrittenByBackend() prometheus.Gauge {
+	return g.new(
+		prometheus.GaugeOpts{
+			Name:        "postgresql_buffers_backend",
 			Help:        "Number of buffers written directly by a backend",
 			ConstLabels: g.labels,
 		},
-		`
-			SELECT buffers_backend
-			FROM pg_stat_bgwriter
-		`,
+		"SELECT buffers_backend FROM pg_stat_bgwriter",
 	)
 }
