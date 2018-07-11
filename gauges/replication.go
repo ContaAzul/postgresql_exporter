031ff15a3cb219ee8713e1a88573283fd47aeba1
merencia <lucas.merencia@gmail.com>
2018-07-11 15:29:34 -0300

    Measuring replication delay in seconds and in bytes

--- gauges/replication.go
+++ gauges/replication.go
@@ -51,30 +51,33 @@ func (g *Gauges) StreamingWALs() prometheus.Gauge {
 	)
 }
 
-// ReplicationLag returns a prometheus gauge for the database replication
-// lag in milliseconds
-func (g *Gauges) ReplicationLag() prometheus.Gauge {
+// ReplicationDelay returns a prometheus gauge for the database replication
+// lag in seconds
+func (g *Gauges) ReplicationDelay() prometheus.Gauge {
+	return g.new(
+		prometheus.GaugeOpts{
+			Name:        "postgresql_replication_delay",
+			Help:        "Dabatase replication delay in seconds",
+			ConstLabels: g.labels,
+		},
+		"SELECT coalesce(extract(epoch from now() - pg_last_xact_replay_timestamp()), 0) AS replication_delay",
+	)
+}
+
+// ReplicationDelayInBytes returns a prometheus gauge for the database replication
+// lag in bytes
+func (g *Gauges) ReplicationDelayInBytes() prometheus.Gauge {
 	version := postgres.Version(g.version())
 
 	return g.new(
 		prometheus.GaugeOpts{
-			Name:        "postgresql_replication_lag",
-			Help:        "Dabatase replication lag",
+			Name:        "postgresql_replication_delay_in_bytes",
+			Help:        "Dabatase replication delay in bytes",
 			ConstLabels: g.labels,
 		},
 		fmt.Sprintf(`
-			SELECT COALESCE(
-				CASE
-					WHEN pg_is_in_recovery() is true
-					THEN
-					CASE
-						WHEN %s() = %s()
-						THEN 0
-					ELSE
-						EXTRACT (EPOCH FROM now() - pg_last_xact_replay_timestamp())
-					END
-				END
-			, 0)`,
+			SELECT COALESCE(ABS(%s(%s(), %s())), 0) AS replication_delay_bytes`,
+			version.WalLsnDiffFunctionName(),
 			version.LastWalReceivedLsnFunctionName(),
 			version.LastWalReplayedLsnFunctionName(),
 		),
