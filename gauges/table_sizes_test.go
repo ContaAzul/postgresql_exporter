79fa7d71b419ad69a8c223e6fc65b47cbf42d5dd
Diogo Nicoleti <diogo.nicoleti@gmail.com>
2018-07-16 17:57:31 -0300

    Add table size metric tests

---
+++ gauges/table_sizes_test.go
@@ -0,0 +1,22 @@
+package gauges
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTableSizes(t *testing.T) {
+	var assert = assert.New(t)
+	db, gauges, close := prepare(t)
+	defer close()
+	dropTestTable := createTestTable(t, db)
+	defer dropTestTable()
+
+	var metrics = evaluate(t, gauges.TableSizes())
+	assert.Len(metrics, 3)
+	for _, metric := range metrics {
+		assertEqual(t, 0, metric)
+	}
+	assertNoErrs(t, gauges)
+}
