e634cc0e5fd6b3b3938b657b346910c77cc64fa9
Marcos Bernardelli <bernardelli.marcos@gmail.com>
2019-09-19 17:48:52 -0300

    Using different slots

--- gauges/logical_replication_test.go
+++ gauges/logical_replication_test.go
@@ -2,6 +2,7 @@ package gauges
 
 import (
 	"database/sql"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,7 +13,7 @@ func TestReplicationSlotStatus(t *testing.T) {
 	var assert = assert.New(t)
 	db, gauges, close := prepare(t)
 	defer close()
-	dropTestLogicalReplicationSlot := createTestLogicalReplicationSlot(t, db)
+	dropTestLogicalReplicationSlot := createTestLogicalReplicationSlot("test_status", t, db)
 	defer dropTestLogicalReplicationSlot()
 	var metrics = evaluate(t, gauges.ReplicationSlotStatus())
 	assert.Len(metrics, 1)
@@ -24,19 +25,19 @@ func TestReplicationSlotLagInMegabytes(t *testing.T) {
 	var assert = assert.New(t)
 	db, gauges, close := prepare(t)
 	defer close()
-	dropTestLogicalReplicationSlot := createTestLogicalReplicationSlot(t, db)
+	dropTestLogicalReplicationSlot := createTestLogicalReplicationSlot("test_lag", t, db)
 	defer dropTestLogicalReplicationSlot()
 	var metrics = evaluate(t, gauges.ReplicationSlotLagInMegabytes())
 	assert.Len(metrics, 1)
-	assertGreaterThan(t, -1, metrics[0])
+	assertEqual(t, 0, metrics[0])
 	assertNoErrs(t, gauges)
 }
 
-func createTestLogicalReplicationSlot(t *testing.T, db *sql.DB) func() {
-	_, err := db.Exec("SELECT * FROM pg_create_logical_replication_slot('integration_test', 'test_decoding');")
+func createTestLogicalReplicationSlot(slotName string, t *testing.T, db *sql.DB) func() {
+	_, err := db.Exec(fmt.Sprintf("SELECT * FROM pg_create_logical_replication_slot('%s', 'test_decoding');", slotName))
 	require.NoError(t, err)
 	return func() {
-		_, err := db.Exec("SELECT pg_drop_replication_slot('integration_test');")
+		_, err := db.Exec(fmt.Sprintf("SELECT pg_drop_replication_slot('%s');", slotName))
 		assert.New(t).NoError(err)
 	}
 }
